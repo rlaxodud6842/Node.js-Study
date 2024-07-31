dd9fb2ee5611371a7a6e72457d5914bc70202bed
rlaxodud6842 <rlaxodud6842@naver.com>
2024-07-31 19:54:12 +0900

    Post 방식 공부

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
+const path = require('path');
 // 얘들은 모듈이다. import격인가봄. url이라는 이름으로 사용하겠다.
+// pm2 start main.js --watch
 function templateHTML(title, list,body){
   return `
         <!doctype html>
@@ -13,6 +16,7 @@ function templateHTML(title, list,body){
         <body>
           <h1><a href="/">WEB</a></h1>
           ${list}
+          <a href="/create">Create</a>
           ${body}
           </p>
         </body>
@@ -35,7 +39,7 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    console.log(queryData.id);
+    console.log(pathname);
 
     if(pathname === '/'){
       if(queryData.id === undefined){
@@ -46,7 +50,7 @@ var app = http.createServer(function(request,response){
         var template = templateHTML(title,list,`<h2>${title}</h2>${description}`);
         response.writeHead(200);
         response.end(template);
-        });
+        })
       }else{
         fs.readdir('./data',function(err,filelist){
           fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
@@ -58,9 +62,42 @@ var app = http.createServer(function(request,response){
           });
         }); 
       } 
+    }else if(pathname === '/create') {
+      fs.readdir('./data',function(err,filelist){
+        var title = 'Web - Create';
+        var list = templateList(filelist);
+        var template = templateHTML(title,list,`<form action="http://localhost:3000/create_process" method = "post"> 
+          <p>
+              <input type = "text" name ="title" placeholder = "Put the title"></p>
+          <p>
+              <textarea name = "description" placeholder = "Put the discription"></textarea>
+          </p>
+          <p>
+              <input type = 'submit'>
+          </p>
+      </form>
+      `);
+        response.writeHead(200);
+        response.end(template);
+        })
+    }else if (pathname === '/create_process'){
+      var body = '';
+      
+      request.on('data',function(data){
+        body += data;
+      });
+
+      request.on('end',function(){
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+
+      });
+      response.writeHead(200);
+      response.end('Success');
     }else{
       response.writeHead(404);
-      response.end(template);
+      response.end('Not Found');
     }
 
     
